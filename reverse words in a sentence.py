dbd214c43e8c322bccc5557b7233212a965b3087
Bhargavi0326 <52918329+Bhargavi0326@users.noreply.github.com>
2019-09-10 16:56:05 +0530

    Update reverse words in a sentence.py

--- reverse words in a sentence.py
+++ reverse words in a sentence.py
@@ -1,6 +1,4 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> S=input()
+S=input()
 li=S.split()
 x= (li[::-1])
 for i in range(2):
